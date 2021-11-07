20b536473b286aa3b44c71eb23eefe7df0bf61a2
antonioferreiraSA <ferreiratoni112@gmail.com>
2021-11-07 20:40:16 +0200

    changing bg-color

--- client/src/components/PeopleList/peopleList.styles.ts
+++ client/src/components/PeopleList/peopleList.styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const PeopleListContainer = styled.div`
+  background-color: #f0ebeb;
   margin-top: 10px;
   display: flex;
   flex-direction: column nowrap;
